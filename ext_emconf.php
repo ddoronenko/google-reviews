4047c6c7e226d2b052f2a6cb3785e1c2c2445a56
Dmytro Doronenko <dmytro.doronenko@711media.de>
2020-12-06 22:30:15 +0200

    Remove deprecated configurations

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,21 +4,15 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Google Review',
     'description' => 'Simple TYPO3 plugin for Google Reviews output.',
     'category' => 'fe',
-    'version' => '1.0.0',
-    'module' => '',
+    'version' => '1.0.1',
     'state' => 'alpha',
-    'uploadfolder' => false,
-    'createDirs' => '',
     'clearcacheonload' => true,
-    'author' => '',
+    'author' => 'Dmytro Doronenko',
     'author_email' => '',
     'author_company' => '',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [],
+        ]
     ],
 ];
