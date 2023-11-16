401b0c99044e58baebe4f7b394bf275f3c8b8f74
Dmytro Doronenko <dmytro.doronenko@711media.de>
2023-11-16 13:47:23 +0200

    Release v3.0.0

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Google Reviews',
+    'description' => 'TYPO3 plugin displays five (most-relevant) Google reviews according to selected Place ID.
+        Extension provides flexible configurations for better user experience.',
+    'category' => 'fe',
+    'version' => '2.0.0',
+    'state' => 'stable',
+    'clearcacheonload' => true,
+    'author' => 'Dmytro Doronenko',
+    'author_email' => '',
+    'author_company' => '',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.4.99',
+        ]
+    ],
+];
